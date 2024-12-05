174aa6a6b86b71645075144b04499e47474f1a48
djtranisme <djtran2007@gmail.com>
2024-12-05 18:04:40 -0500

    update again
    tbh idk what im doing but im doing something

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+
 
 public class Robot extends TimedRobot {
 
@@ -13,6 +15,10 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    final XboxController xboxController = new XboxController(0);
+    final PWMSparkMax leftMotors = new PWMSparkMax(0);
+    final PWMSparkMax rightMotors = new PWMSparkMax(1); //Note change id's once we know what they are on the kitbot
+
   }
 
   @Override
@@ -22,46 +28,41 @@ public class Robot extends TimedRobot {
   public void autonomousInit() {
   }
 
-  /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
   }
 
-  /** This function is called once when teleop is enabled. */
   @Override
   public void teleopInit() {
-    XboxController XboxController = new XboxController(0);
 
 
   }
 
-  /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    
+
   }
 
-  /** This function is called once when the robot is disabled. */
   @Override
   public void disabledInit() {}
 
-  /** This function is called periodically when disabled. */
+
   @Override
   public void disabledPeriodic() {}
 
-  /** This function is called once when test mode is enabled. */
+
   @Override
   public void testInit() {}
 
-  /** This function is called periodically during test mode. */
+
   @Override
   public void testPeriodic() {}
 
-  /** This function is called once when the robot is first started up. */
+
   @Override
   public void simulationInit() {}
 
-  /** This function is called periodically whilst in simulation. */
+
   @Override
   public void simulationPeriodic() {}
 }
